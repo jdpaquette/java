fbaba16783474dc4856019b4a65eedeb7779b978
Joseph Paquette <josephdpaquette@gmail.com>
2019-05-06 20:23:59 -0400

    commit

--- module1/hello.java
+++ module1/hello.java
@@ -2,6 +2,9 @@ public class Hello
 {
     public static void main(String[] args) 
     {
-        System.out.println("hello, world!");
+        System.out.println("XYZ Company");
+        System.out.println("8900 U.S. Hwy 14")
+        System.out.println("Crystal Lake, IL 60014");
+        System.out.println("First Java App");
     }
 }
