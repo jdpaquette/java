2b3cdbbac8bc04818bcf0d954c62d1ba637c0cf2
Joseph D. Paquette <josephdpaquette@gmail.com>
2019-09-15 15:55:59 -0400

    fix errors

--- module4/CountWords.java
+++ module4/CountWords.java
@@ -4,35 +4,36 @@
 import java.util.*;
 public class CountWords
 {
-    String string;
-    Scanner in = new Scanner(System.in);
-    char character;
-    int val;
-    int wordCount = 0;
-    int wordLength;
-    boolean prevPunctuation = false;
-    System.out.println("Enter a phrase >> ");
-    string = in.nextLine();
-    wordLength = str.length();
-    
-    for(val = 0; val < wordLength; val++)
+    public static void main(String[] args)
     {
-       character = str.charAt(x); //set the punctuation
-       if(character == ' ' || character == '.' || character == ';' || character == ',' ||
-          character == '!' || character == '-')
-       {
-          ++wordCount;
-          if(prevPunctuation)
-             --wordCount;
-          prevPunctuation = true;
-       }
-       else
-          prevPunctuation = false;
-    }
-    if(!prevPunctuation)
-       ++wordCount;
+        String string;
+        Scanner in = new Scanner(System.in);
+        char character;
+        int val;
+        int wordCount = 0;
+        int wordLength;
+        boolean prevPunctuation = false;
+        System.out.println("Enter a phrase >> ");
+        string = in.nextLine();
+        wordLength = string.length();
+        for(val = 0; val < wordLength; val++)
+        {
+            character = string.charAt(val); //set the punctuation
+            if(character == ' ' || character == '.' || character == ';' || character == ',' ||
+                character == '!' || character == '-')
+            {
+                ++wordCount;
+                if(prevPunctuation)
+                    --wordCount;
+                prevPunctuation = true;
+            }
+            else
+                prevPunctuation = false;
+        }
+        if(!prevPunctuation)
+            ++wordCount;
     
-    // final output   
-    System.out.println("There are " + wordCount + " words in this phrase");
- }
+        // final output   
+        System.out.println("There are " + wordCount + " words in this phrase");
+     }
 }       
