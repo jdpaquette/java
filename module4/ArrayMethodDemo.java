f0d38c3a9a9b1570f2baefb20420a766393cda5d
Joseph D. Paquette <josephdpaquette@gmail.com>
2019-09-15 16:10:34 -0400

    first ch8ex4

---
+++ module4/ArrayMethodDemo.java
@@ -0,0 +1,18 @@
+//Farrell - Java Programming - Chapter 8 - Problem 4
+// Working with Arrays
+//created / revised by Joe Paquette Sept. 15th 2019
+
+public class ArrayMethodDemo
+{
+    public static void main (String args[])
+    {
+        int[] values = {43, 29, 11, 26, 49, 32, 18, 12};
+        int maxVal = 30;
+        display(values);
+        displayReverse(values);
+        displaySum(values);
+        displayLessThan(values, max);
+        diplayHigherThanAverage(values);
+    }
+    
+}
