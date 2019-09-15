b8c32876dd1638169a5e5b56920d598f952f5775
Joseph D. Paquette <josephdpaquette@gmail.com>
2019-09-15 13:53:34 -0400

    added ch7ex4

---
+++ module4/README.md
@@ -0,0 +1,5 @@
+Farrell Chapter 7 Programming Exercises 4 & 5
+
+Farrell Chapter 8 Programming Exercises 4
+
+Farrell Chapter 9 Programming Exercises 6
--- module4/ValidatePassword.java
+++ module4/ValidatePassword.java
@@ -2,12 +2,50 @@
 //created / revised by Joe Paquette Sept. 15th 2019
 
 import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
 public class ValidatePassword
 {
     public static void main(String[] args)
     {
         String aString;
         int stringSize;
-            
+        final int numberUpperLetters = 2;
+        final int numberLowerLetters = 3;
+        final int numberNumerals = 1;
+        int upperTotal = 0;
+        int lowerTotal = 0;
+        int numeralTotal = 0;
+        int letterTotal = 0;
+        Scanner in = new Scanner(System.in);
+        int numberSpaces = 0;
+        System.out.print("Enter a string >> ");
+        aString = in.nextLine();
+        stringSize = aString.length();
+        
+        for(int i = 0; i < stringSize; i++)
+        {
+            char ch = aString.charAt(i);
+            if(Character.isUpperCase(ch))
+                upperTotal++;
+            else
+                if(Character.isLowerCase(ch))
+                    lowerTotal++;
+                else
+                    if(Character.isDigit(ch))
+                        numeralTotal++;        
+        }
+        if(upperTotal >= numberUpperLetters && lowerTotal >= numberLowerLetters && numeralTotal >= numberNumerals)
+            System.out.println("Your Password is Valid");
+        else
+        {
+            System.out.println("Your password is missing the following:");
+            if(upperTotal < numberUpperLetters)
+                System.out.println("uppercase letters");
+            if(lowerTotal < numberLowerLetters)
+                System.out.println("lowercase letters");
+            if(numeralTotal < numberNumerals)
+                System.out.println("numerals");
+        }
     }
 }
