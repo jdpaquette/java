3e27d7536f7c2b50394de7dd792c754216574470
Joseph D. Paquette <josephdpaquette@gmail.com>
2019-09-15 16:49:38 -0400

    first commit ch9ex6

---
+++ module4/TimesAndInstructors.java
@@ -0,0 +1,42 @@
+//Farrell - Java Programming - Chapter 9 - Problem 6
+// Advanced Array Topics
+//created / revised by Joe Paquette Sept. 15th 2019
+
+import javax.swing.*;
+class TimesAndInstructors
+{
+    public static void main(String[] args) 
+    {
+        courses[0][0] = "COM-140";
+        courses[0][1] = "TR 9:30am";
+        courses[0][2] = "McGinnis";
+        courses[1][0] = "COM-140";
+        courses[1][1] = "MWF 8:30am";
+        courses[1][2] = "Adams";
+        courses[2][0] = "COM-207";
+        courses[2][1] = "MWF 11:30am";
+        courses[2][2] = "Reagan";
+        courses[3][0] = "COM-207";
+        courses[3][1] = "TR 11:00am";
+        courses[3][2] = "Banisakher";
+        courses[4][0] = "COM-209";
+        courses[4][1] = "TR 2:00pm";
+        courses[4][2] = "Banisakher";
+        courses[5][0] = "COM-302";
+        courses[5][1] = "MWF 11:30am";
+        courses[5][2] = "Adams";
+        courses[6][0] = "COM-327";
+        courses[6][1] = "MWF 9:30am";
+        courses[6][2] = "Bal";
+        courses[7][0] = "COM-330";
+        courses[7][1] = "TR 8:00am";
+        courses[7][2] = "Omar";
+        courses[8][0] = "COM-340";
+        courses[8][1] = "MWF 1:30pm";
+        courses[8][2] = "Reagan";
+        courses[9][0] = "COM-405";
+        courses[9][1] = "MWF 2:30pm";
+        courses[9][2] = "Kirshnan";
+    
+    }  
+}
