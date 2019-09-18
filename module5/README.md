719bfc3fccc999f10adff600348dedf99a7c6c39
Joseph Paquette <joseph.paquette@saintleo.edu>
2019-09-18 16:09:08 -0400

    errors ch10ex3

---
+++ module5/README.md
@@ -0,0 +1,3 @@
+Farrell Chapter 10 Programming Exercises 4 & 4
+
+Farrell Chapter 1 Programming Exercises 3
---
+++ module5/TeeShirt.java
@@ -0,0 +1,43 @@
+//Farrell - Java Programming - Chapter 10 - Problem 3
+// Intro to Inheritance
+//created / revised by Joe Paquette Sept. 18th 2019
+
+public class TeeShirt
+{
+    private int orderNumber;
+    private String shirtSize;
+    private double shirtPrice;
+    private String color;
+    public void setOrderNumber (int number)
+    {
+        orderNumber = number;
+    }
+    public void setSize(String size)
+    {
+        shirtSize = size;
+        if(shirtSize == "XXL" || shirtSize == "XXXL")
+            shirtPrice = 22.99;
+        else
+            shirtPrice = 19.99;
+    }
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+    public int getOrderNumber()
+    {
+        return orderNumber;
+    }
+    public String getSize()
+    {
+        return size;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public double getPrice()
+    {
+        return shirtPrice;
+    }
+}
