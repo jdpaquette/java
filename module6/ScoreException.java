48e58358ea9546177be6becfdb41e4ec75e4d42f
Joseph D. Paquette <josephdpaquette@gmail.com>
2019-09-29 14:00:38 -0400

    CH12 EX8 first commit

---
+++ module6/ScoreException.java
@@ -0,0 +1,11 @@
+//Farrell - Java Programming - Chapter 12 - Problem 8a
+// Exception Handling
+//created / revised by Joe Paquette Sept. 29th 2019
+
+public class ScoreException extends Exception
+{
+   public ScoreException(String s)
+   {
+      super(s);
+   }
+}
