20fbf060a80dfdf1affe50558782383d45ff36a8
Joseph D. Paquette <josephdpaquette@gmail.com>
2019-10-06 10:05:17 -0400

    first commit CH14EX3

---
+++ module7/JFacts.java
@@ -0,0 +1,9 @@
+//Farrell - Java Programming - Chapter 14 - Problem 3
+// Intro to Swing Components
+//created / revised by Joe Paquette Oct. 6th, 2019
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class JFacts extends JFrame implements ActionListener
