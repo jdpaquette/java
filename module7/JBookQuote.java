83934db0877e6c5ee52f9e5d9913535a42262080
Joseph D. Paquette <josephdpaquette@gmail.com>
2019-10-16 22:34:19 -0400

    final error correction

--- module7/JBookQuote.java
+++ module7/JBookQuote.java
@@ -1,6 +1,4 @@
-//Farrell - Java Programming - Chapter 14 - Problem 1
-// Intro to Swing Components
-//created / revised by Joe Paquette Oct. 6th, 2019
+
 
 import javax.swing.*;
 import java.awt.*;
